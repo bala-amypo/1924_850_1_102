dd761823a1f5a86f2f44270693a461b65d4e4e2c
bala-amypo <bala@amypo.in>
2025-12-20 15:09:43 +0000

    User commit

--- src/main/java/com/example/demo/security/CustomerUserDetails.java
+++ src/main/java/com/example/demo/security/CustomerUserDetails.java
@@ -1,5 +1,53 @@
 package com.example.demo.security;
 
-public class CustomerUserDetails {
+import java.util.Collection;
+import java.util.Collections;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class CustomUserDetails implements UserDetails {
+
+    private String username;
+    private String password;
+
+    public CustomUserDetails(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
--- src/main/java/com/example/demo/security/CustomerUserDetailsService.java
+++ src/main/java/com/example/demo/security/CustomerUserDetailsService.java
@@ -1,5 +1,17 @@
 package com.example.demo.security;
 
-public class CustomerUserDetailsService {
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+
+        return new CustomUserDetails(username, "password");
+    }
 }
