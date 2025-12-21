153c173a2ac3fbe67c252b831c5753c0c2906ad5
bala-amypo <bala@amypo.in>
2025-12-21 15:09:04 +0000

    User commit

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -18,14 +18,16 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String name;
 
-    @Column(unique = true, nullable = false)
+    @Column(nullable = false, unique = true)
     private String email;
 
     @Column(nullable = false)
     private String password;
 
+    @Column(nullable = false)
     private String role;
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
